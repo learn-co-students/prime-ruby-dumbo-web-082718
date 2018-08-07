2051f402012a84778e65d0f3e2a54b24c2fd9e85
Steven Paulino <stevenpaulino1@hotmail.com>
2018-08-07 01:28:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+require 'pry'
+
+def prime? (number)
+  if number < 2 # 2 is a prime therefore the benchmark. 2 is greater than 1,0, and negatives that procede it 
+    return false
+  else (2..number/2).none? { |i| number % i == 0 }
+  
+end
+end
