ca32bca15dc984d6488287d07fc4a5caf16c6133
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2018-08-07 18:43:50 -0400

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+
+  count = 2
+
+  while count < num
+    if (num % count) == 0
+      return false
+    end
+      count += 1
+    end
+  true
+end
