50b317bcdb56ea98a061bb6f58b14e8433191966
Kat Bartels <k@gothfem.me>
2018-08-18 14:19:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(x)
+  if x < 2
+    false
+  else
+    (2..x-1).each do |check|
+      if x % check == 0
+       return false
+       end
+    end
+    true
+  end
+end
