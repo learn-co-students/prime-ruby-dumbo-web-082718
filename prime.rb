2aae59733415fece6babbc2d62643dde6c6fa44a
Andy <wang.andy32@gmail.com>
2018-08-19 10:37:14 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+  if num < 0
+    num = num.abs
+  end
+  if num == 2
+    return true
+  elsif num == 1 || num == 0
+    return false
+  else
+    for int in 2...num
+      if num%int == 0
+        return false
+      end
+    end
+  end
+  true
+end
