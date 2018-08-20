18340ab1d9c5d0949dee1b01925224e0206f767b
Nasser Sanou <nassersanou23@gmail.com>
2018-08-20 08:01:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(int)
+  if int < 2
+    return false 
+  end 
+  
+  
+  (2..int.size).each do |factor|
+   
+    if int % factor == 0 
+     return false 
+     end 
+   end 
+  else true 
+end 
+prime? 4
+
+
+
+
