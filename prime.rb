de54ff90b0563f95ed5011946f350eaea74ee9ad
Nasser Sanou <nassersanou23@gmail.com>
2018-08-20 08:08:14 +0000

    prime without 'Math'-Done

--- prime.rb
+++ prime.rb
@@ -3,18 +3,12 @@ def prime?(int)
   if int < 2
     return false 
   end 
-  
-  
-  (2..int.size).each do |factor|
-   
-    if int % factor == 0 
-     return false 
-     end 
-   end 
-  else true 
-end 
-prime? 4
 
+  (2...int).each do |factor|
+    if int % factor == 0
+      return false
+    end
+  end
+  return true
 
-
-
+end
